0b75f8172022e1148b43bb39b8726bc10eb3b583
ruscoder <rusmiligamer@gmail.com>
2014-05-23 19:01:40 +0800

    test4

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [digest "1.4.4"]
+                 [ring "1.2.1"]
                  [compojure "1.1.6"]
                  [clj-time "0.7.0"]
                  [sonian/carica "1.1.0" :exclusions [[cheshire]]]
@@ -17,4 +18,5 @@
                  [com.taoensso/carmine "2.6.0"]]
   :plugins [[lein-ring "0.6.3"]]
   :main service.core
+  :min-lein-version "2.3.4"
   :ring {:handler service.core/app})
