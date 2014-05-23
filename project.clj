03c0211b77915adb9e510e8d9d890e6e56165896
ruscoder <rusmiligamer@gmail.com>
2014-05-23 18:37:44 +0800

    test3

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [digest "1.4.4"]
-                 [ring "1.2.1"]
                  [compojure "1.1.6"]
                  [clj-time "0.7.0"]
                  [sonian/carica "1.1.0" :exclusions [[cheshire]]]
