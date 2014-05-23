b92f329278b2d8944ec1f1bce425835a6b9497d2
ruscoder <rusmiligamer@gmail.com>
2014-05-23 18:36:18 +0800

    test 2

--- project.clj
+++ project.clj
@@ -1,14 +1,21 @@
-(defproject client "0.1.0-SNAPSHOT"
+(defproject service "0.1.0-SNAPSHOT"
+  :description "Mini twitter"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2202"]
-                 [enfocus "2.1.0-SNAPSHOT"]
-                 [cljs-ajax "0.2.3"]
-                 [secretary "1.1.0"]]
-  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.3"]]}}
-  :cljsbuild {
-    :builds [{
-        :source-paths ["src"]
-        :compiler {
-          :output-to "resources/js/main.js"
-          :optimizations :simple
-          :pretty-print true}}]})
+                 [digest "1.4.4"]
+                 [ring "1.2.1"]
+                 [compojure "1.1.6"]
+                 [clj-time "0.7.0"]
+                 [sonian/carica "1.1.0" :exclusions [[cheshire]]]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [korma "0.3.1"]
+                 [mysql/mysql-connector-java "5.1.30"]
+                 [fogus/ring-edn "0.2.0"]
+                 [http-kit "2.1.18"]
+                 [com.taoensso/carmine "2.6.0"]]
+  :plugins [[lein-ring "0.6.3"]]
+  :main service.core
+  :ring {:handler service.core/app})
